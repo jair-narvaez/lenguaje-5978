1cf300d793dddd19b0a379ae376848754e3dbf33
Jair Narvaez <jair.fer.narvaez@gmail.com>
2022-08-08 09:27:45 -0500

    variables locales

--- C++/funciones/tallerfunciones.cpp
+++ C++/funciones/tallerfunciones.cpp
@@ -1,19 +1,23 @@
 #include <iostream>
 using namespace std;
 void llenaletra(char v[]){
-
+int i,j;
 
 }
 
 void ordena(char v[]){
-
+int i,j;
 }
 
 void buscarletra(char v[]){
-
+int i,j;
 }
 
 main(){
+char letras [100];
+llenaletra(letras);
+ordena(letras);
+buscarletra(letras);
 
 
 }
